a4055b711f73a7fab7a1c5a59958dfb33a6a33d4
Charlie Root <bawdyanarchist@protonmail.com>
2023-09-30 13:33:53 -0500

    VMs integration and microimprovements

--- zroot/usr/local/lib/quBSD/msg-stat.sh
+++ zroot/usr/local/lib/quBSD/msg-stat.sh
@@ -1,6 +1,43 @@
 #!/bin/sh
 
-usage() { cat << ENDOFUSAGE 
+get_msg_() { 
+
+	local _message="$1"
+	local _pass_cmd="$2"
+
+	case "$_message" in
+	_1) cat << ENDOFMSG
+
+ENDOFMSG
+	;;	
+	_2) cat << ENDOFMSG
+
+ENDOFMSG
+	;;	
+	_3) cat << ENDOFMSG
+
+ENDOFMSG
+	;;
+	_4) cat << ENDOFMSG
+
+ENDOFMSG
+	;;
+	_5) cat << ENDOFMSG
+
+ENDOFMSG
+	;;
+	esac
+
+	case $_pass_cmd in 
+		usage_0) usage ; exit 0 ;;
+		usage_1) usage ; exit 1 ;;
+		exit_0)  exit 0 ;;
+		exit_1)  exit 1 ;;
+		*) : ;;
+	esac
+}
+
+usage() { cat << ENDOFUSAGE
 
 qb-stat: List status of all jails 
 
