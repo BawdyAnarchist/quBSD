df42e5d748690b9ff9f432b86218835098f073d8
Charlie Root <bawdyanarchist@protonmail.com>
2023-10-10 18:45:12 -0500

    Handling/message improvements for no dataset

--- zroot/usr/local/lib/quBSD/msg-destroy.sh
+++ zroot/usr/local/lib/quBSD/msg-destroy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-get_msg_destroy() { 
+get_msg_destroy() {
 
 	local _message="$1"
 	local	_pass_cmd="$2"
@@ -12,47 +12,54 @@ get_msg_destroy() {
 Exiting, no changes were made.
 
 ENDOFMSG
-	;;	
+	;;
 	_2) cat << ENDOFMSG
 
-ERROR: < $JAIL > has the \`no_destroy protection flag' set in 
-       jailmap.conf.  Change flag to \`false', and run again. 
+ERROR: < $JAIL > has the \`no_destroy protection flag' set in
+       jailmap.conf.  Change flag to \`false', and run again.
 ENDOFMSG
-	;;	
+	;;
 	_3) cat << ENDOFMSG
-#[[OPEN ENTRY, NO MESSAGES HERE]]
+
+ERROR: Must specificy a <jail> to destroy
 ENDOFMSG
 	;;
 	_4) cat << ENDOFMSG
-WARNING! < $JAIL > is a $CLASS 
+WARNING! < $JAIL > is a $CLASS
 ENDOFMSG
 	;;
 	_5) cat << ENDOFMSG
 WARNING! This will destroy the following datasets:
 ENDOFMSG
 	;;
+	_5_1) cat << ENDOFMSG
+
+ALERT: No datasets to destroy. Would you like to remove any
+ENDOFMSG
+echo -e "       lingering parts/pieces of jail/VM? (Y/n): \c"
+	;;
 	_6) cat << ENDOFMSG
 
-< $zrootDestroy > 
+< $zrootDestroy >
                           Totaling: $zrootSize of ondisk data
 ENDOFMSG
 	;;
 	_7) cat << ENDOFMSG
 
-< $zusrDestroy > 
+< $zusrDestroy >
                           Totaling: $zusrSize of ondisk data
 
 NOTE! Beware the difference between zroot jails, which are
-rootjails and clones; VS zusr jails which contain user data 
+rootjails and clones; VS zusr jails which contain user data
 ENDOFMSG
 	;;
-	_8) 
+	_8)
 		echo -e "\n          To continue, type yes: \c"
 	;;
 
 	esac
 
-	case $_pass_cmd in 
+	case $_pass_cmd in
 		usage_0) usage ; exit 0 ;;
 		usage_1) usage ; exit 1 ;;
 		exit_0)  exit 0 ;;
@@ -63,16 +70,16 @@ ENDOFMSG
 
 usage() { cat << ENDOFUSAGE
 
-qb-destroy: Destroy <jail> and purge associated configs:
+qb-destroy: Destroy <jail/VM> and purge associated configs:
             jail.conf ; jailmap.conf ; zfs datasets
 
 Even if jail is partially created, this command will purge it.
 
-There is a protection mechanism for all jails. jailmap.conf	
+There is a protection mechanism for all jails/VMs. jailmap.conf
 has a parameter called: \`no_destroy', which defaults to true.
 Must manually edit this setting to false, before qb-destroy
 
-Usage: qb-destroy [-h] <jail>"
+Usage: qb-destroy [-h] <jail/VM>"
    -h: (h)elp. Outputs this help message"
 
 ENDOFUSAGE
