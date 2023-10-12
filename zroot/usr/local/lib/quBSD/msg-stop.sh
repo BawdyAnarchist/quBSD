b1dd239df76f9cbd4dfb1c4739feae28923a3f3e
Charlie Root <bawdyanarchist@protonmail.com>
2023-10-11 23:04:45 -0500

    Add a force, non-graceful stop option.

--- zroot/usr/local/lib/quBSD/msg-stop.sh
+++ zroot/usr/local/lib/quBSD/msg-stop.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-get_msg_stop() { 
+get_msg_stop() {
 
 	local _message="$1"
 	local _pass_cmd="$2"
@@ -14,45 +14,55 @@ ERROR: Two instances of qb-stop or qb-start are already running.
 
 $(pgrep -fl '/bin/sh /usr/local/bin/qb-st(art|op)')
 ENDOFMSG
-	;;	
+	;;
 	_e1) cat << ENDOFMSG
 
 ERROR: Conflicting options. Specify only one: [-a|-A|-f]
 ENDOFMSG
-	;;	
+	;;
 	_e2) cat << ENDOFMSG
 
 ERROR: Conflicting options. Either use [-E <file>], or use
-       [-e] and type a list of exclusion jails as positional 
+       [-e] and type a list of exclusion jails as positional
        parameters (aka, at the end of the command).
 ENDOFMSG
-	;;	
+	;;
 	_e3) cat << ENDOFMSG
 
 ERROR: One or more jails appear to be hung and havent stopped.
-       Check /var/log/quBSD.sh for details, and check jls 
+       Check /var/log/quBSD.sh for details, and check jls
        list to manually kill all `jail -R` operations.
 
        NOTE - No restarts were attempted, due to error.
 ENDOFMSG
-	;;	
+	;;
 	_e4) cat << ENDOFMSG
 
 ERROR: One or more jails appear to be hung and havent stopped.
        Check /var/log/quBSD.sh for details, and check process
        list to manually kill all jail stop operations.
 ENDOFMSG
-	;;	
+	;;
 	_e5) cat << ENDOFMSG
 
-ERROR: [-t <timeout>] must be integer from 5 to 600. Caution, 
+ERROR: [-t <timeout>] must be integer from 5 to 600. Caution,
        choose a timeout appropriate for number of starts,
        longer if starting multiple gateways/clients in a row
 ENDOFMSG
-	;;	
+	;;
+	_m1) cat << ENDOFMSG
+
+ALERT: Force stopping jails/VMs is non-graceful and not preferred.
+       For example, passthru'd PCI devices attached to a VM will
+       likely become unavailable until after a system reboot.
+       Use this only as a last resort for misbehaving containers.
+
+ENDOFMSG
+echo -e "Continue? (Y/n): \c"
+	;;
 	esac
 
-	case $_pass_cmd in 
+	case $_pass_cmd in
 		usage_0) usage ; exit 0 ;;
 		usage_1) usage ; exit 1 ;;
 		exit_0)  exit 0 ;;
@@ -64,24 +74,26 @@ ENDOFMSG
 usage() { cat << ENDOFUSAGE
 
 !IMPORTANT; qb-stop MUST be used when stopping multiple
-jails in parallel to avoid race conditions and errors. 
+jails in parallel to avoid race conditions and errors.
 
 qb-stop [-a|-A|-f <file>] [-e|-E <file>] [-r]
-        [-t <timeout>] < jail list >
+        [-t <timeout>] <jail_list>
+qb-stop [-F] <jail_list>
 
    If no [-a|-A|-f] is specified, < jail list > (positional
-   parameters) at the end are assumed to be jail stops. 
+   parameters) at the end are assumed to be jail stops.
 
    -a: (a)uto. Stop all jails tagged with autostop in jmap
        This is the default behavior if no opts are specified.
-   -A: (A)ll. Start ALL valid jails on the system. 
-   -e: (e)xclude. Start [-a|-A] jails, but exclude jails 
-       passed via positional parameters <jail list> 
+   -A: (A)ll. Start ALL valid jails on the system.
+   -e: (e)xclude. Start [-a|-A] jails, but exclude jails
+       passed via positional parameters <jail list>
    -E: (E)xclude. Start [-a|-A|< jail list >], but
        exclude any jails listed in <file>
    -f: (f)ile. Use a file with a list of jails to stop.
+   -F: (F)orce non-graceful termination of jail/VM.
    -h: (h)elp. Outputs this help message.
-   -r: (r)estart. Restart all jails after stopping them. 
+   -r: (r)estart. Restart all jails after stopping them.
    -t: (t)timeout in secs, to wait for jail stops before error
        *This is auto calculated, so normally dont change this.
 
