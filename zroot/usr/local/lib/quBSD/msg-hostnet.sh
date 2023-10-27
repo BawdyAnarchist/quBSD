345e08a79bf6587f7e83bb9cf32905384a6957a7
Charlie Root <bawdyanarchist@protonmail.com>
2023-10-27 16:58:43 -0600

    Rename variable, and implement improved check.

--- zroot/usr/local/lib/quBSD/msg-hostnet.sh
+++ zroot/usr/local/lib/quBSD/msg-hostnet.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-get_msg_hostnet() { 
+get_msg_hostnet() {
 
 	local _message="$1"
 	local _pass_cmd="$2"
@@ -8,34 +8,34 @@ get_msg_hostnet() {
 	case "$_message" in
 	_1) cat << ENDOFMSG
 
-ERROR: Options are mutually exclusive. Chose one. 
+ERROR: Options are mutually exclusive. Chose one.
 ENDOFMSG
-	;;	
+	;;
 	_2) cat << ENDOFMSG
 
 ERROR: Must specify an action [-d|-u]
 ENDOFMSG
-	;;	
+	;;
 	_3) cat << ENDOFMSG
 
-ERROR: Tunnel < $JAIL > failed to start. 
+ERROR: Tunnel < $JAIL > failed to start.
        For more info, see: /var/log/quBSD.log
 ENDOFMSG
 	;;
 	_6) cat << ENDOFMSG
 
-ERROR: [-t] time must be a number. 
+ERROR: [-t <timeout>] must be a number greater than 0.
 ENDOFMSG
-	;;	
+	;;
 	_7) cat << ENDOFMSG
 
 WARNING: User opted to remove the network timeout. Host
          will keep this network connection indefintely.
 ENDOFMSG
-	;;	
+	;;
 	esac
 
-	case $_pass_cmd in 
+	case $_pass_cmd in
 		usage_0) usage ; exit 0 ;;
 		usage_1) usage ; exit 1 ;;
 		exit_0)  exit 0 ;;
@@ -50,7 +50,7 @@ qb-hostnet: Connect host to outside internet.
 
 Usage: hostnet [-h][-d|-u][-t <time_in_seconds>]
    -h: (h)elp. Outputs this help message
-   -d: (d)own. Remove connectivity; set pf to block all 
+   -d: (d)own. Remove connectivity; set pf to block all
    -u: (u)p. Brings up connectivity as specified in QMAP
 
    -t: (t)ime before connection is automatically removed.
@@ -62,8 +62,8 @@ Usage: hostnet [-h][-d|-u][-t <time_in_seconds>]
        To disable default timeout and maintain indefinite
        host connection, use qb-hostnet -u -t 0
 
-   When host is connected, qb-stat will print a large 
-   flashing warning message in red letters. 
+   When host is connected, qb-stat will print a large
+   flashing warning message in red letters.
 
 ENDOFUSAGE
 }
