50178adc11a9f2e325210a128d2aec5079ebc3a8
Charlie Root <bawdyanarchist@protonmail.com>
2024-10-19 19:14:40 -0600

    Modularize into proper functions

--- Install/ports_installer.sh
+++ Install/ports_installer.sh
@@ -4,41 +4,80 @@
 # and which doesn't modify any pre-existing files outside of /usr/local 
 # This function will become the ports/pkg install script, with a secondary installer required later. 
 
-# Repo location for files copy
-_SHARE="/usr/local/share/"
-_REPO="/usr/local/share/quBSD"
+download_base() {
+	# Download the latest base.txz
+	BASETXZ="/usr/local/freebsd-dist/base.txz"
+	rel=$(freebsd-version -u | sed -E 's/([0-9]+\.[0-9]+).*$/\1-RELEASE/')
+	link_txz="https://ftp.freebsd.org/releases/$(uname -m)/${rel}/base.txz"
+	dl_size=$(echo "$(fetch -s $link_txz) / 1048576" | bc)
+	[ -d "${BASETXZ%/*}" ] || mkdir ${BASETXZ%/*} 
+	fetch -o $BASETXZ $link_txz &
+	DL_PID="$!"
+}
 
-# Install dependencies - possibly also xpra, xephyr, and doas in later revisions
-pkg install bhyve-firmware tmux
+fetch_repo() {
+	# Fetch repo to /usr/local/share
+	REPO="/usr/local/share/quBSD"
+	fetch -o ${REPO%/*} https://github.com/BawdyAnarchist/quBSD/archive/refs/heads/main.zip
+	unzip -qd ${REPO%/*} ${REPO%/*}/main.zip
+	mv ${REPO%/*}/quBSD-main ${REPO}
+	rm ${REPO%/*}/main.zip
+}
 
-# Fetch repo to /usr/local/share
-fetch -o ${_SHARE} https://github.com/BawdyAnarchist/quBSD/archive/refs/heads/main.zip
-unzip -qd ${_SHARE} ${_SHARE}/main.zip
-mv ${_SHARE}/quBSD-main ${_REPO}
-rm ${_SHARE}/main.zip
+copy_repo() {
+	# Make sure the required directories exist
+	[ -e /usr/local/etc/quBSD ] || mkdir -p /usr/local/etc/quBSD
+	[ -e /usr/local/lib/quBSD ] || mkdir -p /usr/local/lib/quBSD
+	[ -e /usr/local/etc/rc.d ]  || mkdir -p /usr/local/etc/rc.d   
+	[ -e /boot/loader.conf.d ]  || mkdir -p /boot/loader.conf.d
+	[ -e /etc/cron.d ]          || mkdir -p /etc/cron.d
 
-# Make sure the required directories exist
-[ -e /usr/local/etc/quBSD ] || mkdir -p /usr/local/etc/quBSD
-[ -e /usr/local/lib/quBSD ] || mkdir -p /usr/local/lib/quBSD
-[ -e /usr/local/etc/rc.d ]  || mkdir -p /usr/local/etc/rc.d   
-[ -e /boot/loader.conf.d ]  || mkdir -p /boot/loader.conf.d
-[ -e /etc/cron.d ]          || mkdir -p /etc/cron.d
+	# Copy files to their directories 
+	cp -a ${REPO}/zroot/usr/local/etc/quBSD/* /usr/local/etc/quBSD/
+	cp -a ${REPO}/zroot/usr/local/bin/*       /usr/local/bin/
+	cp -a ${REPO}/zroot/usr/local/lib/quBSD/* /usr/local/lib/quBSD/
+	cp -a ${REPO}/zroot/usr/local/etc/rc.d/*  /usr/local/etc/rc.d/
+	cp -a ${REPO}/zroot/boot/loader.conf.d/*  /boot/loader.conf.d/
+}
 
-# Copy files to their directories 
-cp -a ${_REPO}/zroot/usr/local/etc/quBSD/* /usr/local/etc/quBSD/
-cp -a ${_REPO}/zroot/usr/local/bin/*       /usr/local/bin/
-cp -a ${_REPO}/zroot/usr/local/lib/quBSD/* /usr/local/lib/quBSD/
-cp -a ${_REPO}/zroot/usr/local/etc/rc.d/*  /usr/local/etc/rc.d/
-cp -a ${_REPO}/zroot/boot/loader.conf.d/*  /boot/loader.conf.d/
+modify_files() {
+	# Check for AMD CPU, and add it to the loader file 
+	dmesg | grep -Eqs "^CPU.*AMD" && echo -e "# This machine has an AMD CPU\nhw.vmm.amdvi.enable=\"1\"" \
+		>> /boot/loader.conf.d/qubsd_loader.conf
 
-# Check for AMD CPU, and add it to the loader file 
-dmesg | grep -Eqs "^CPU.*AMD" && echo -e "# This machine has an AMD CPU\nhw.vmm.amdvi.enable=\"1\"" \
-	>> /boot/loader.conf.d/qubsd_loader.conf
+	# Based on pciconf class=network, find the first interface listed in rc.conf and assume it's the primary nic 
+	_nics=$(pciconf -lv | grep -B3 "= network" | grep -Eo "^[[:alnum:]]+" | grep -v none)
+	for _nic in $_nics ; do
+		grep -E "^ifconfig_${_nic}" /etc/rc.conf \
+			&& sed "s/#nic=.*/nic=${_nic}/" ${REPO}/Install/install.conf \
+			&& break
+	done
+}
 
-# Based on pciconf class=network, find the first interface listed in rc.conf and assume it's the primary nic 
-_nics=$(pciconf -lv | grep -B3 "= network" | grep -Eo "^[[:alnum:]]+" | grep -v none)
-for _dev in $_nics ; do
-	grep -E "^ifconfig_${_dev}" /etc/rc.conf \
-		&& sed "s/#nic=.*/nic=${_nic}/" ${_REPO}/Install/install.conf \
-		&& break
-done
+verify_base_download() {
+	while ps -p $DL_PID > /dev/null 2>&1 ; do
+		sleep 1
+
+		# Calculate % completion of download, only if the file exists
+		[ -e "$BASETXZ" ] \
+			&& dl_prog=$(ls -lh $BASETXZ | awk '{print $5}' | sed -E 's/.$//') \
+			&& dl_pct=$(echo "scale=2 ; ( ${dl_prog} + 0 ) / ${dl_size} * 100" | bc)
+		printf "\033[1A"
+		printf "\033[K"
+		echo "  $BASETXZ     ${dl_pct%%.*}% of ${dl_size}M"
+	done			
+}
+
+main() {
+	download_base
+	
+	# Install dependencies - possibly also xpra, xephyr, and doas in later revisions
+	pkg install bhyve-firmware tmux
+
+	fetch_repo
+	copy_repo	
+	modify_files
+	verify_base_download
+}
+
+main
