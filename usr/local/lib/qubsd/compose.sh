fc3de06738a1ffb3a209eacc564ba7bc648218e5
Bawdy <bawdyanarchist@protonmail.com>
2026-03-16 21:35:51 -0600

    Bugfix: For missing child datasets, RT_CTX must be updated after cloning the parent

--- usr/local/lib/qubsd/compose.sh
+++ usr/local/lib/qubsd/compose.sh
@@ -60,15 +60,18 @@ resolve_persist_snapname() {
 
 # Caller should be careful if deconfliction via $1 (_pfx) is necessary
 compose_root_reclone_cmds() {
-    local _fn="compose_root_reclone_cmds" _pfx="$1" _rootenv _snap _die _r_mnt _r_dset _pfxloc="rrc_"
+    local _fn="compose_root_reclone_cmds" _pfx="$3" _pfxloc="rrc_"
+    local  _cell _rt_ctx _rootenv _snap _die _r_mnt _r_dset _r_zfs_mnt
+    assert_args_set 2 "$1" "$2" && _cell="$1" _rt_ctx="$2" || $(THROW $?)
 
     # Compose the local vars based on their prefixes
     _rootenv=$(ctx_get ${_pfx}ROOTENV)
-    _r_mnt=$(ctx_get ${_pfx}R_MNT)
+    _r_zfs=$(ctx_get ${_pfx}R_ZFS)
     _r_dset=$(ctx_get ${_pfx}R_DSET)
+    _r_mnt=$(ctx_get ${_pfx}R_MNT)
 
     # Need the root dataset of the rootenv, to choose the snapshot
-    ctx_bootstrap_cell $_rootenv $_pfxloc || eval $(THROW 1 _generic "< $_rootenv > bootstrap failed")
+    ctx_bootstrap_cell $_rootenv $_pfxloc || eval $(THROW $? _generic "< $_rootenv > bootstrap failed")
 
     _snap=$(resolve_rootenv_snapname $(ctx_get ${_pfxloc}R_DSET))
     case $? in
@@ -79,19 +82,27 @@ compose_root_reclone_cmds() {
             ;;
     esac
 
-    [ "$_r_mnt" ] && _CMD_DESTROY_ROOT="zfs destroy -rRf $_r_dset"
+    # R_MNT is null, then RT_CTX needs updated after clone. Otherwise, RT_CTX is fine, just destroy/reclone
+    if [ "$_r_mnt" ] ; then
+        _CMD_DESTROY_ROOT="zfs destroy -rRf $_r_dset"
+    else
+        _r_zfs_mnt="$(query_zfs_mountpoint $_r_zfs)/$_cell"
+        _CMD_UPDATE_R_MNT_RTCTX="sed -i '' -E \"s|^(R_MNT=\\\")|\1$_r_zfs_mnt|\" $_rt_ctx"
+    fi
     _CMD_CLONE_ROOT="zfs clone -o qb:autosnap=false $_snap $_r_dset"
 }
 
 # $1 required. Caller should be careful if deconfliction via $2 (_pfx) is necessary
 compose_persist_reclone_cmds() {
-    local _fn="compose_persist_reclone_cmds" _cell _pfx="$2" _snap _die _p_mnt _p_dset _pfxloc="prc_"
-    assert_args_set 1 "$1" && _cell="$1" || $(THROW 1)
+    local _fn="compose_persist_reclone_cmds" _pfx="$3" _pfxloc="prc_"
+    local _cell _rt_ctx _snap _die _p_mnt _p_dset _p_zfs_mnt
+    assert_args_set 2 "$1" "$2" && _cell="$1" && _rt_ctx="$2" || $(THROW $?)
 
     # Compose the local vars based on their prefixes
     _template=$(ctx_get ${_pfx}TEMPLATE)
-    _p_mnt=$(ctx_get ${_pfx}P_MNT)
+    _p_zfs=$(ctx_get ${_pfx}P_ZFS)
     _p_dset=$(ctx_get ${_pfx}P_DSET)
+    _p_mnt=$(ctx_get ${_pfx}P_MNT)
 
     # Need the persist dataset of the template, to choose the snapshot
     ctx_bootstrap_cell $_template $_pfxloc || eval $(THROW 1 _generic "< $_template > bootstrap failed")
@@ -104,7 +115,13 @@ compose_persist_reclone_cmds() {
             ;;
     esac
 
-    [ "$_p_mnt" ] && _CMD_DESTROY_PERSIST="zfs destroy -rRf $_p_dset"
+    # P_MNT is null, then RT_CTX needs updated after clone. Otherwise, RT_CTX is fine, just destroy/reclone
+    if [ -z "$_p_mnt" ] ; then
+        _p_zfs_mnt="$(query_zfs_mountpoint $_p_zfs)/$_cell"
+        _CMD_UPDATE_P_MNT_RTCTX="sed -i '' -E \"s|^(P_MNT=\\\")|\1$_p_zfs_mnt|\" $_rt_ctx"
+    else
+        _CMD_DESTROY_PERSIST="zfs destroy -rRf $_p_dset"
+    fi
     _CMD_CLONE_PERSIST="zfs clone -o qb:autosnap=false $_snap $_p_dset"
     _CMD_FIX_PW="fix_freebsd_pw $_cell $(ctx_get $_pfxloc) $_p_mnt"
 }
