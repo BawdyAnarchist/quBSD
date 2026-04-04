760439ade25d05fd801d3536cdc105e320bd352a
Bawdy <bawdyanarchist@protonmail.com>
2026-04-04 16:00:47 -0600

    Better handling/handoff/integration of the assertion vs validation boundaries. We prefer to reduce context awareness at the lower scopes, while providing the capacity for ERR message composition up the stack. Thus, assert failure codes are now passed through the validation functions, while unique validation logic receives its own codes. All PARAM validation failures now produce user-usable messages that enhance the context of assertion failures.

--- usr/local/lib/qubsd/assert.sh
+++ usr/local/lib/qubsd/assert.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-################################  SECTION 1: GENERAL FORMAT CHECKS  ################################
-
 assert_args_set() {
     local _fn="assert_args_set"
     local _require="$1" ; shift
@@ -17,11 +15,34 @@ assert_args_set() {
     return 0
 }
 
-assert_bytesize() {
-    local _fn="assert_bytesize"
-    echo "$1" | grep -Eqs "^[[:digit:]]+(T|t|G|g|M|m|K|k)\$" || eval $(THROW 19 _invalid bytesize $1)
+assert_bhyveopts() {
+    local _fn="assert_bhyveopts" _val="$1"
+    _val=$(echo "$_val" | sed -E 's/^-//')   # Remove the leading dash
+
+    # Only includes bhyve opts with no argument
+    echo "$_val" | grep -Eqs -- '^[AaCDeHhPSuWwxY]+$' || eval $(THROW 32 ${_fn}_1)
+
+    # No duplicate characters
+    [ "$(echo "$_val" | fold -w1 | sort | uniq -d | wc -l)" -gt 0 ] && eval $(THROW 33 ${_fn}_2)
+
+    return 0
 }
 
+# Impossible to fully assert bhyve_custm parameter user inputs. But some guarantees can be provided
+assert_bhyve_custm() {
+    local _fn="assert_bhyve_custm" _val="$1"
+    echo "$_val" | grep -Eqv "^[a-zA-Z0-9 \-/_:,.=+]*$" || eval $(THROW 31 $_fn)
+}
+
+assert_bool_tf() {
+    local _fn="assert_bool_tf"
+    echo "$1" | grep -Eqs "^(true|TRUE|FALSE|false)\$" || eval $(THROW 12 $_fn)
+}
+
+assert_bytesize() {
+    local _fn="assert_bytesize"
+    echo "$1" | grep -Eqs "^[[:digit:]]+(T|t|G|g|M|m|K|k)\$" || eval $(THROW 19 $_fn)
+}
 normalize_bytesize() {
     local _fn="normalize_bytesize" _val="$1" _raw
     _raw=$(echo $_val | sed -nE "s/.\$//p")
@@ -33,42 +54,58 @@ normalize_bytesize() {
     esac
 }
 
-assert_bool_tf() {
-    local _fn="assert_bool_tf"
-    echo "$1" | grep -Eqs "^(true|TRUE|FALSE|false)\$" || eval $(THROW 12 $_fn)
-}
-
 assert_cellname() {
     local _fn="assert_cellname" _val="$1"
     # Jail must start with :alnum: and afterwards, have only _ or - as special chars
     echo "$_val" | grep -E -- '^[[:alnum:]]([-_[:alnum:]])*[[:alnum:]]$' \
-        | grep -Eqv '(--|-_|_-|__)' || eval $(THROW 16 $_fn $_val)
+        | grep -Eqv '(--|-_|_-|__)' || eval $(THROW 16 $_fn)
+
+    case $_val in
+        none|qubsd|host) eval $(THROW 16 ${_fn}_2) ;;
+    esac
 }
 
-assert_new_cellname() {
-    local _fn="assert_cellname" _val="$1"
+assert_cellname_new() {
+    local _fn="assert_cellname_new" _val="$1"
 
     # Trigger words that shouldn't be used
     case $_val in
-        none|qubsd|host) eval $(THROW 16 _invalid2 cellname $_val "Cannot be 'none' or 'qubsd'") ;;
+        none|qubsd|host) eval $(THROW 16 assert_cellname_2)
     esac
 
     # Jail must start with :alnum: and afterwards, have only _ or - as special chars
     echo "$_val" | grep -E -- '^[[:alnum:]]([-_[:alnum:]])*[[:alnum:]]$' \
-        | grep -Eqv '(--|-_|_-|__)' || eval $(THROW 16 $_fn $_val)
+        | grep -Eqv '(--|-_|_-|__)' || eval $(THROW 16 assert_cellname)
+}
+
+assert_class() {
+    local _fn="assert_class"
+    echo_grep -qd , "$CLASSES" "$1" || eval $(THROW 18 $_fn $CLASSES)
+}
+
+assert_cpuset() {
+    local _fn="assert_cpuset"
+    # Test for negative numbers and dashes in the wrong place
+    echo "$1" | grep -Eq "(,,+|--+|,-|-,|,[ \t]*-|^[^[:digit:]])" && eval $(THROW 21 $_fn)
+    return 0
 }
 
 assert_dataset_name() {
     local _fn="assert_dataset_name" _val="$1"
 
     echo "$_val" | grep -Eq "^[a-zA-Z][a-zA-Z0-9_.:-]*(/[a-zA-Z0-9_.:-]+)*$" \
-        || eval $(THROW 17 $_fn $_val)
+        || eval $(THROW 17 $_fn)
     return 0
+}
+
+assert_devfs_rule() {
+    local _fn="assert_devfs_rule"
+    assert_int_comparison -g 0 "$1" || eval $(THROW 26 $_fn)
 }
 
 assert_integer() {
     local _fn="assert_integer"
-    echo "$1" | grep -Eqs -- '^(-|[0-9])[0-9]*$' || eval $(THROW 13 $_fn $1)
+    echo "$1" | grep -Eqs -- '^(-|[0-9])[0-9]*$' || eval $(THROW 13 $_fn)
 }
 
 # Checks that $1 is integer, and comparison boundaries are valid
@@ -95,70 +132,6 @@ assert_int_comparison() {
     return 0
 }
 
-# Ensures that $1 is part of the static PARAMS_ALL list
-assert_param() {
-    local _fn="assert_param"
-    echo_grep -qd , "$PARAMS_ALL" "$1" || eval $(THROW 10 $_fn "$1")
-}
-
-# getopts needs improved. Guarantee $GETOPTS was provided.
-assert_optarg() {
-    local _fn="assert_optarg"
-    [ -z "$OPTARG" ] && eval $(THROW 25 $_fn)
-    echo "$OPTARG" | grep -Eqs -- "^-" && eval $(THROW 25 $_fn)
-    unset OPTARG  # Must unset, or stale value lingers (coz it's local-scoped)
-}
-
-assert_ptype() {
-    local _fn="assert_type" _val="$1"
-    echo_grep -q "base BASE jail JAIL vm VM" "$_val" || eval $(THROW 24)
-}
-
-assert_time_format() {
-    local _fn="assert_time_format" _val="$1"
-    echo "$_val" | grep -Eqs "^[1-9]+[0-9]*(s|m|H|D|W|Y)\$" || eval $(THROW 20 $_fn)
-}
-
-
-###################################  SECTION 2: CELL PARAMETERS  ###################################
-
-# Impossible to fully assert bhyve_custm parameter user inputs. But some guarantees can be provided
-assert_bhyve_custm() {
-    local _fn="assert_bhyve_custm" _val="$1"
-    echo "$_val" | grep -Eqv "^[a-zA-Z0-9 \-/_:,.=+]*$" || eval $(THROW 31 $_fn)
-}
-
-assert_bhyveopts() {
-    local _fn="assert_bhyveopts" _val="$1"
-    _val=$(echo "$_val" | sed -E 's/^-//')   # Remove the leading dash
-
-    # Only includes bhyve opts with no argument
-    echo "$_val" | grep -Eqs -- '^[AaCDeHhPSuWwxY]+$' || eval $(THROW 32 ${_fn}1 BHYVEOPTS $_val)
-
-    # No duplicate characters
-    [ "$(echo "$_val" | fold -w1 | sort | uniq -d | wc -l)" -gt 0 ] \
-        && eval $(THROW 33 ${_fn}2 BHYVEOPTS $_val)
-
-    return 0
-}
-
-assert_class() {
-    local _fn="assert_class"
-    echo_grep -qd , "$CLASSES" "$1" || eval $(THROW 18 _invalid CLASS $1)
-}
-
-assert_cpuset() {
-    local _fn="assert_cpuset"
-    # Test for negative numbers and dashes in the wrong place
-    echo "$1" | grep -Eq "(,,+|--+|,-|-,|,[ \t]*-|^[^[:digit:]])" && eval $(THROW 21 $_fn $1)
-    return 0
-}
-
-assert_devfs_rule() {
-    local _fn="assert_devfs_rule"
-    assert_int_comparison -g 0 "$1" || eval $(THROW $?)
-}
-
 # Optional $2: (_extra), to ensure that the last bit in the IP is not '1'.  It's required for the
 # automated gateway/client management. But some assert_ipv4() calls might not care about this.
 assert_ipv4() {
@@ -178,17 +151,31 @@ assert_ipv4() {
     # Ensures that each number is in the proper range
     echo "$_val" \
         | grep -Eqs "^[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+\.[[:digit:]]+/[[:digit:]]+\$" \
-        || eval $(THROW 15 _invalid2 IPV4 "$_val" "Use CIDR notation (with subnet)")
+        || eval $(THROW 15 $_fn)
 
     # Ensures that each digit is within the proper range
     { [ "$_a0" -ge 0 ] && [ "$_a0" -le 255 ] && [ "$_a1" -ge 0 ] && [ "$_a1" -le 255 ] \
         && [ "$_a2" -ge 0 ] && [ "$_a2" -le 255 ] && [ "$_a3" -ge 0 ] && [ "$_a3" -le 255 ] \
         && [ "$_a4" -ge 0 ] && [ "$_a4" -le 32 ] ;} \
-        || eval $(THROW 15 _invalid2 IPV4 "$_val" "Use CIDR notation (with subnet)")
+        || eval $(THROW 15 _fail2 IPV4 "$_val" "Use CIDR notation (with subnet)")
 
     # With the $_extra flag, reserve a.b.c.1 (ending in .1) for the gateway
-    [ "$_a3" = "1" ] && eval $(THROW 15 ${_fn}_2 $_value)
+    [ "$_a3" = "1" ] && eval $(THROW 15 ${_fn}_2)
     return 0
+}
+
+# Guarantee that $OPTARG is actually present (and not just another dash-option)
+assert_optarg() {
+    local _fn="assert_optarg"
+    [ -z "$OPTARG" ] && eval $(THROW 25 $_fn)
+    echo "$OPTARG" | grep -Eqs -- "^-" && eval $(THROW 25 $_fn)
+    unset OPTARG  # Must unset, or stale value lingers (coz it's local-scoped)
+}
+
+# Ensures that $1 is part of the static PARAMS_ALL list
+assert_param() {
+    local _fn="assert_param"
+    echo_grep -qd , "$PARAMS_ALL" "$1" || eval $(THROW 10 $_fn $PARAMS_ALL)
 }
 
 assert_ppt() {
@@ -197,11 +184,16 @@ assert_ppt() {
         || eval $(THROW 33 $_fn $1)
 }
 
+assert_ptype() {
+    local _fn="assert_type" _val="$1"
+    echo_grep -q "base BASE jail JAIL vm VM" "$_val" || eval $(THROW 24)
+}
+
 assert_schg() {
     local _fn="assert_schg"
     case $1 in
         none|sys|all) return 0 ;;
-        *) eval $(THROW 22 _invalid2 SCHG $1 "Must be <none|sys|all>") ;;
+        *) eval $(THROW 22 _fail2 SCHG $1 "Must be <none|sys|all>") ;;
     esac
 }
 
@@ -209,17 +201,21 @@ assert_seclvl() {
     local _fn="assert_seclvl"
     case $1 in
         none|-1|-0|0|1|2|3) return 0 ;;
-        *) eval $(THROW 23 _invalid2 SECLVL $1 "Must be <none|-1|0|1|2|3>") ;;
+        *) eval $(THROW 23 _fail2 SECLVL $1 "Must be <none|-1|0|1|2|3>") ;;
     esac
 }
 
 assert_taps() {
     local _fn="assert_taps"
-    assert_int_comparison -g 0 -- "$1" || eval $(THROW 34 _invalid2 TAPS $1 "Require: integer >= 0")
+    assert_int_comparison -g 0 -- "$1" || eval $(THROW 34 _generic "Require: Integer greater than 0")
+}
+assert_time_format() {
+    local _fn="assert_time_format" _val="$1"
+    echo "$_val" | grep -Eqs "^[1-9]+[0-9]*(s|m|H|D|W|Y)\$" || eval $(THROW 20 $_fn)
 }
 
 assert_vcpus() {
     local _fn="assert_vcpus"
-    assert_int_comparison -G 0 -- "$1" || eval $(THROW 35 _invalid2 VCPUS $1 "Require: integer > 0")
+    assert_int_comparison -G 0 -- "$1" || eval $(THROW 35 _generic "Require: integer > 0")
 }
 
--- usr/local/lib/qubsd/validate.sh
+++ usr/local/lib/qubsd/validate.sh
@@ -3,37 +3,37 @@
 validate_param_autostart() {
     local _fn="validate_param_autostart"
     [ -z "$_value" ] && return 0  # Not critical
-    assert_bool_tf $_value || eval $(THROW 141)
+    assert_bool_tf $_value || eval $(THROW $? _invalid $_param $_value)
 }
 
 validate_param_autosnap() {
     local _fn="validate_param_autosnap"
     [ -z "$_value" ] && return 0  # Not critical
-    assert_bool_tf $_value || eval $(THROW 142)
+    assert_bool_tf $_value || eval $(THROW $? _invalid $_param $_value)
 }
 
 validate_param_backup() {
     local _fn="validate_param_backup"
     [ -z "$_value" ] && return 0  # Not critical
-    assert_bool_tf $_value || eval $(THROW 143)
+    assert_bool_tf $_value || eval $(THROW $? _invalid $_param $_value)
 }
 
 validate_param_bhyveopts() {
     local _fn="validate_param_bhyveopts"
     [ -z "$_value" ] && return 0  # Not critical
-    assert_bhyveopts $_value || eval $(THROW 144)
+    assert_bhyveopts $_value || eval $(THROW $? _invalid $_param $_value)
 }
 
 validate_param_bhyve_custm() {
     local _fn="validate_param_bhyve_custm"
     [ -z "$_value" ] && return 0  # Not critical
-    assert_bhyve_custm $_value || eval $(THROW 145)
+    assert_bhyve_custm $_value || eval $(THROW $? _invalid $_param $_value)
     return 0
 }
 
 validate_param_class() {
     local _fn="validate_param_class"
-    assert_class $_value || eval $(THROW 146)
+    assert_class $_value || eval $(THROW $? _invalid $_param $_value)
 }
 
 validate_param_control() {
@@ -41,10 +41,10 @@ validate_param_control() {
 
     [ "$_value" = "none" ] && return 0
 
-    assert_cellname "$_value" || eval $(THROW 147)
+    assert_cellname "$_value" || eval $(THROW $? _invalid $_param $_value)
     [ "$_level" -le 1 ] && return 0
 
-    ctx_bootstrap_cell $_value "val_" || eval $(THROW 147 _invalid CJAIL $_value)
+    ctx_bootstrap_cell $_value "val_" || eval $(THROW 147 _invalid $_param $_value)
     return 0
 }
 
@@ -53,20 +53,24 @@ validate_param_cpuset() {
     [ -z "$_value" ] && return 0  # Not critical
     [ "$_value" = "none" ] && return 0  # Must come first or will THROW cpuset
 
-    assert_cpuset "$_value" || eval $(THROW 148)
+    assert_cpuset "$_value" || eval $(THROW $? _invalid $_param $_value)
     [ "$_level" -le 2 ] && return 0
 
-    quiet cpuset -l "$_value" || eval $(THROW 148 $_fn $_value)
+    quiet cpuset -l "$_value" || eval $(THROW 148 $_fn $_param $_value)
     return 0
 }
 
 validate_param_devfs_rule() {
     local _fn="validate_devfs_rule"
-    assert_devfs_rule $_value || eval $(THROW 145)
+    assert_devfs_rule $_value || eval $(THROW $? _invalid $_param $_value)
+    [ "$_level" -le 1 ] && return 0
+
+    grep -Eqs "^\[.*=$_value\]" || eval $(THROW 171 $_fn $_param $_value $DEVFS)
+
     return 0
 }
 
-validate_param_envsync() { ##########  STUB  FOR  NOW  ################################################################
+validate_param_envsync() { ##########  STUB  FOR  NOW  #############
     local _fn="validate_param_envsync"
     # [some test] || eval $(THROW 149 $_fn)
 }
@@ -77,10 +81,10 @@ validate_param_gateway() {
     [ -z "$_value" ] && return 0  # Not critical
     [ "$_value" = "none" ] && return 0
 
-    assert_cellname "$_value" || eval $(THROW 150)
+    assert_cellname "$_value" || eval $(THROW $? _invalid $_param $_value)
     [ "$_level" -le 1 ] && return 0
 
-    ctx_bootstrap_cell $_value "val_" || eval $(WARN _invalid GATEWAY $_value)
+    ctx_bootstrap_cell $_value "val_" || eval $(WARN _invalid $_param $_value)
 }
 
 validate_param_ipv4() {
@@ -88,7 +92,7 @@ validate_param_ipv4() {
 
     case $_value in
         ''|none|auto|DHCP) return 0 ;;
-        *) assert_ipv4 $_value || eval $(THROW 151) ;;
+        *) assert_ipv4 $_value || eval $(THROW $? _invalid $_param $_value) ;;
     esac
     [ "$_level" -le 1 ] && return 0
 
@@ -97,8 +101,8 @@ validate_param_ipv4() {
     [ "$_type" = "VM" ] && eval $(WARN $_fn) && return 0  # No addtl checks for impotent IPV4
 
     # Jails only. Pull relevant gateway information required for config/runtime guarantees
-    _gw=$(query_cell_param "$_cell" GATEWAY) || eval $(THROW 151)
-    _gw_type=$(query_cell_type $_gw) || eval $(THROW 151)
+    _gw=$(query_cell_param "$_cell" GATEWAY) || eval $(THROW 151 _invalid $_param $_value)
+    _gw_type=$(query_cell_type $_gw) || eval $(THROW 151 _invalid $_param $_value)
     _cli_confs=$(query_gw_client_configs $_gw)
 
     # Static config checks
@@ -118,7 +122,7 @@ validate_param_jconf() {
     local _fn="validate_param_jconf" _type
     _type=$(ctx_get ${_pfx}TYPE)
     [ "$_type" = "VM" ] && return 0
-    is_path_exist -f $_value || eval $(THROW 168)
+    is_path_exist -f $_value || eval $(THROW $? _invalid $_param $_value)
 }
 
 validate_param_maxmem() {
@@ -127,30 +131,32 @@ validate_param_maxmem() {
     [ -z "$_value" ] && return 0  # Not critical
     [ "$_value" = "none" ] && return 0  # Must come first or will THROW bytesize
 
-    assert_bytesize $_value || eval $(THROW 152)
+    assert_bytesize $_value || eval $(THROW $? _invalid $_param $_value)
     [ "$_level" -le 2 ] && return 0
 
     query_sysmem
     _bytes=$(normalize_bytesize $_value)
-    assert_int_comparison -l "$SYSMEM" -- $_bytes || eval $(WARN $_fn $_value $_bytes $SYSMEM)
+    assert_int_comparison -l "$SYSMEM" -- $_bytes \
+        || eval $(WARN $_fn $_bytes $_param $_value $SYSMEM)
 }
 
 validate_param_memsize() {
     local _fn="validate_param_memsize" _bytes
 
-    assert_bytesize $_value || eval $(THROW 153)
+    assert_bytesize $_value || eval $(THROW $? $_param $_value)
     [ "$_level" -le 2 ] && return 0
 
     query_sysmem
     _bytes=$(normalize_bytesize $_value)
-    assert_int_comparison -l "$SYSMEM" -- $_bytes || eval $(THROW 153 $_fn $_value $_bytes $SYSMEM)
+    assert_int_comparison -l "$SYSMEM" -- $_bytes \
+        || eval $(THROW 153 $_fn $_bytes $_param $_value $SYSMEM)
 }
 
 validate_param_mtu() {
     local _fn="validate_param_mtu"
 
     [ -z "$_value" ] && return 0  # Not critical
-    assert_integer "$_value" || eval $(THROW 154)
+    assert_integer "$_value" || eval $(THROW $? _invalid $_param $_value)
     [ "$_level" -le 1 ] && return 0
 
     # THROW for IPv4 spec violations. WARN for IPv6 spec violation and > typical jumbo packet size
@@ -161,7 +167,7 @@ validate_param_mtu() {
 validate_param_no_destroy() {
     local _fn="validate_param_no_destroy"
     [ -z "$_value" ] && return 0  # Not critical
-    assert_bool_tf $_value || eval $(THROW 155)
+    assert_bool_tf $_value || eval $(THROW $? _invalid $_param $_value)
 }
 
 validate_param_ppt() {
@@ -170,63 +176,63 @@ validate_param_ppt() {
     [ -z "$_value" ] && return 0  # Not critical
     [ "$_value" = "none" ] && return 0
 
-    assert_ppt "$_value" || eval $(THROW 156)
+    assert_ppt "$_value" || eval $(THROW $? _invalid $_param $_value)
     [ "$_level" -le 2 ] && return 0
 
     _value=$(echo $_value | sed "s#/#:#g")
     for _ppt in $_value ; do
         _result=$(hush pciconf -l "pci$_ppt")
-        [ "$_result" ] || eval $(THROW 156 _invalid $_ppt)
+        [ "$_result" ] || eval $(THROW 156 _invalid $_param $_ppt)
     done
     return 0
 }
 
 validate_param_p_dset() {
     local _fn="validate_param_p_dset"
-    validate_dataset_generic || eval $(THROW 170)
+    validate_dataset_generic || eval $(THROW $? _invalid $_param $_value)
 }
 
 validate_param_p_zfs() {
     local _fn="validate_param_p_zfs"
-    validate_dataset_generic || eval $(THROW 157)
+    validate_dataset_generic || eval $(THROW $? _invalid $_param $_value)
 }
 
 validate_param_rootenv() {
     local _fn="validate_param_rootenv"
 
-    assert_cellname "$_value" || eval $(THROW 158)
+    assert_cellname "$_value" || eval $(THROW $? _invalid $_param $_value)
     [ "$_level" -le 1 ] && return 0
 
     # Check the rootenv
-    ctx_bootstrap_cell $_value "val_" || eval $(THROW 158 _invalid ROOTENV $_value)
+    ctx_bootstrap_cell $_value "val_" || eval $(THROW 158 _invalid $_param $_value)
 }
 
 validate_param_r_dset() {
     local _fn="validate_param_r_dset"
-    validate_dataset_generic || eval $(THROW 169)
+    validate_dataset_generic || eval $(THROW $? _invalid $_param $_value)
 }
 
 validate_param_r_zfs() {
     local _fn="validate_param_r_zfs"
-    validate_dataset_generic || eval $(THROW 159)
+    validate_dataset_generic || eval $(THROW $? _invalid $_param $_value)
 }
 
 validate_param_schg() {
     local _fn="validate_param_schg"
     [ -z "$_value" ] && return 0  # Not critical
-    assert_schg $_value || eval $(THROW 160)
+    assert_schg $_value || eval $(THROW $? _invalid $_param $_value)
 }
 
 validate_param_seclvl() {
     local _fn="validate_param_seclvl"
     [ -z "$_value" ] && return 0  # Not critical
-    assert_seclvl $_value || eval $(THROW 161)
+    assert_seclvl $_value || eval $(THROW $? _invalid $_param $_value)
 }
 
 validate_param_taps() {
     local _fn="validate_param_taps"
     [ -z "$_value" ] && return 0  # Not critical
-    assert_taps $_value || eval $(THROW 162)
+    assert_taps $_value || eval $(THROW $? _invalid $_param $_value)
 }
 
 validate_param_template() {
@@ -235,14 +241,14 @@ validate_param_template() {
     [ -z "$_value" ] && return 0  # Not critical
     [ "$_value" = "none" ] && return 0
 
-    assert_cellname "$_value" || eval $(THROW 163)
+    assert_cellname "$_value" || eval $(THROW $? _invalid $_param $_value)
     [ "$_level" -le 1 ] && return 0
 
     # Pivot the check based on CLASS
     _class=$(ctx_get ${_pfx}CLASS)
     case $_class in
         disp*) ctx_bootstrap_cell $_value "val_" \
-                   || eval $(THROW 163 _invalid TEMPLATE $_value) ;;
+                   || eval $(THROW 163 _invalid $_param $_value) ;;
         *)  : ;;  # Not a dispjail
     esac
     return 0
@@ -251,31 +257,31 @@ validate_param_template() {
 validate_param_tmux() {
     local _fn="validate_param_tmux"
     [ -z "$_value" ] && return 0  # Not critical
-    assert_bool_tf $_value || eval $(THROW 164)
+    assert_bool_tf $_value || eval $(THROW $? _invalid $_param $_value)
 }
 
 validate_param_vcpus() {
     local _fn="validate_param_vcpus"
 
-    assert_vcpus $_value || eval $(THROW 165)
+    assert_vcpus $_value || eval $(THROW $? _invalid $_param $_value)
     [ "$_level" -le 2 ] && return 0
 
     query_ncpus
     # Ensure that vpcus doesnt exceed the number of system cpus or bhyve limits
-    { [ "$_value" -gt "$NCPU" ] || [ "$_value" -gt 16 ] ;} && eval $(THROW 165 $_fn $_value $NCPU)
+    { [ "$_value" -gt "$NCPU" ] || [ "$_value" -gt 16 ] ;} && eval $(THROW 165 $_fn $_param $_value $NCPU)
     return 0
 }
 
 validate_param_vnc() {
     local _fn="validate_param_vnc"
     [ -z "$_value" ] && return 0  # Not critical
-    assert_bool_tf $_value || eval $(THROW 166)
+    assert_bool_tf $_value || eval $(THROW $? _invalid $_param $_value)
 }
 
 validate_param_wiremem() {
     local _fn="validate_param_wiremem"
     [ -z "$_value" ] && return 0  # Not critical
-    assert_bool_tf $_value || eval $(THROW 167)
+    assert_bool_tf $_value || eval $(THROW $? _invalid $_param $_value)
 }
 
 ########################################  MISC VALIDATIONS  ########################################
